392554d5c15d1ba7e4d8a16ee66285bfe4bb1c98
Yash Sehgal <yashsehgal@Yashs-MacBook-Air.local>
2022-11-01 01:17:54 +0530

    feat: time duration added to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,20 @@ export default function Hero() {
               <span className="growthlog-animated-gradient">growthlog</span> 
               <span className="emoji-wrapper ml-2">{"🚀"}</span>
             </h2>
+            <p className="hero-section-description text-base font-normal text-gray-600 leading-snug w-fit h-auto flex flex-row items-center justify-start gap-2 mt-6">
+                <span className="inner-content">{"Growth from 2021 till "}</span>
+                <span className="inner-content date-wrapper flex flex-row items-center justify-start gap-1">
+                    <span className="inner-content date_date-wrapper px-2 py-1 rounded-md border border-gray-300 shadow bg-gray-100 text-sm">
+                        {"Date: " + new Date().getDate()}
+                    </span>
+                    <span className="inner-content date_month-wrapper px-2 py-1 rounded-md border border-gray-300 shadow bg-gray-100 text-sm">
+                        {"Month: " + new Date().getMonth()}
+                    </span>
+                    <span className="inner-content date_year-wrapper px-2 py-1 rounded-md border border-gray-300 shadow bg-gray-100 text-sm">
+                        {"Year:" + new Date().getFullYear()}
+                    </span>
+                </span>
+            </p>
         </section>
     )
 }
