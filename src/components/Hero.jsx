0777542f2b57d04a8b8f1d8d290b1892790359ab
Yash Sehgal <yashsehgal@Yashs-MacBook-Air.local>
2022-11-01 00:45:53 +0530

    feat: hero section created

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+
+export default function Hero() {
+    return (
+        <section className="hero-section my-12">
+            <h2 className="text-4xl leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">
+              <span className="growthlog-animated-gradient">growthlog</span> 
+              <span className="emoji-wrapper ml-2">{"🚀"}</span>
+            </h2>
+        </section>
+    )
+}
---
+++ src/dataSource/growth-log.js
@@ -0,0 +1,9 @@
+
+function getGrowthLogData(id) {
+    const growthLog = [
+        {
+            title: "Started building HEKORS"
+        }
+    ];
+    return growthLog[id];
+}
