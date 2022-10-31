48e57f130d18dac1c0e59db5136292ad66a6f3e2
Yash Sehgal <yashsehgal@Yashs-MacBook-Air.local>
2022-11-01 01:37:40 +0530

    perf: growth-data method improved

--- src/dataSource/growth-log.js
+++ src/dataSource/growth-log.js
@@ -1,9 +1,5 @@
 
 function getGrowthLogData(id) {
-    const growthLog = [
-        {
-            title: "Started building HEKORS"
-        }
-    ];
-    return growthLog[id];
+    const growthLog = [];
+    return growthLog.length > 0 ? growthLog[id] : false;
 }
